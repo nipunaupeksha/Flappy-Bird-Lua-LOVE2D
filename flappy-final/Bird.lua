cbf25fff8926de1904bfb6f93b551160f8d94232
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-10-12 21:43:15 +0530

    flappy-4

--- flappy-final/Bird.lua
+++ flappy-final/Bird.lua
@@ -18,5 +18,8 @@ end
 
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
+    if love.keyboard.wasPressed('space') then 
+        self.dy = -5
+    end
     self.y = self.y + self.dy
-end
+end 
--- flappy-final/main.lua
+++ flappy-final/main.lua
@@ -25,6 +25,7 @@ function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
     bird:update(dt)
+    love.keyboard.keysPressed = {}
 end
 
 function love.load()
@@ -35,6 +36,7 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w,h)
@@ -42,8 +44,17 @@ function love.resize(w,h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
     if key == 'escape' then
         love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then 
+        return true
+    else 
+        return false
     end
 end
 
